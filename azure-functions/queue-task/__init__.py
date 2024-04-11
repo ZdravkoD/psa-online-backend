130bc43f9800a97ed4a326ce41ab9a1150820412
ZdravkoD <kfzzdravko@gmail.com>
2024-04-11 12:15:24 +0300

    Update staging function code

--- azure-functions/queue-task/__init__.py
+++ azure-functions/queue-task/__init__.py
@@ -15,7 +15,7 @@ app = func.FunctionApp()
 #                  azservicebus.get_body().decode('utf-8'))
 
 
-@app.route(route="http_trigger", auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="queue-task", auth_level=func.AuthLevel.FUNCTION)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
